ac8e7452d8d3a51de8edfeedfa90b5496a9ccbec
Ashwath Vasudevan <83507067+ash20012003@users.noreply.github.com>
2022-01-02 19:21:26 +0530

    Create randPermutation.cpp

---
+++ randPermutation.cpp
@@ -0,0 +1,44 @@
+
+void swap(int* a, int* b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+// A utility function to print an array
+void printArray(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << "\n";
+}
+
+// A function to generate a random
+// permutation of arr[]
+void randomize(int arr[], int n)
+{
+    // Use a different seed value so that
+    // we don't get same result each time
+    // we run this program
+    srand(time(NULL));
+
+    // Start from the last element and swap
+    // one by one. We don't need to run for
+    // the first element that's why i > 0
+    for (int i = n - 1; i > 0; i--)
+    {
+        // Pick a random index from 0 to i
+        int j = rand() % (i + 1);
+
+        // Swap arr[i] with the element
+        // at random index
+        swap(&arr[i], &arr[j]);
+    }
+}
+int main(){
+int arr[] = {1, 2, 3, 4, 5, 6, 7, 8};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    randomize (arr, n);
+    printArray(arr, n);
+}
