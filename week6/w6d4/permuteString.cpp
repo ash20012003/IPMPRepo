3991ef18cdd513ea314b2d95c15f22bdd2684b7e
Ashwath Vasudevan <83507067+ash20012003@users.noreply.github.com>
2022-01-25 00:01:46 +0530

    Create permuteString.cpp

---
+++ week6/w6d4/permuteString.cpp
@@ -0,0 +1,22 @@
+void permute(string a, int l, int r) 
+{ 
+    // Base case 
+    if (l == r) 
+        cout<<a<<endl; 
+    else
+    { 
+        // Permutations made 
+        for (int i = l; i <= r; i++) 
+        { 
+
+            // Swapping done 
+            swap(a[l], a[i]); 
+
+            // Recursion called 
+            permute(a, l+1, r); 
+
+            //backtrack 
+            swap(a[l], a[i]); 
+        } 
+    } 
+}
