534564ce32b53eeef93aea8ba2f6dfd9457b58ba
Ashwath Vasudevan <83507067+ash20012003@users.noreply.github.com>
2022-02-17 22:06:23 +0530

    Create isPAth.cpp

---
+++ week10/w10d0/isPAth.cpp
@@ -0,0 +1,49 @@
+bool Graph::isReachable(int s, int d)
+{
+    // Base case
+    if (s == d)
+      return true;
+ 
+    // Mark all the vertices as not visited
+    bool *visited = new bool[V];
+    for (int i = 0; i < V; i++)
+        visited[i] = false;
+ 
+    // Create a queue for BFS
+    list<int> queue;
+ 
+    // Mark the current node as visited and enqueue it
+    visited[s] = true;
+    queue.push_back(s);
+ 
+    // it will be used to get all adjacent vertices of a vertex
+    list<int>::iterator i;
+ 
+    while (!queue.empty())
+    {
+        // Dequeue a vertex from queue and print it
+        s = queue.front();
+        queue.pop_front();
+ 
+        // Get all adjacent vertices of the dequeued vertex s
+        // If a adjacent has not been visited, then mark it visited
+        // and enqueue it
+        for (i = adj[s].begin(); i != adj[s].end(); ++i)
+        {
+            // If this adjacent node is the destination node, then
+            // return true
+            if (*i == d)
+                return true;
+ 
+            // Else, continue to do BFS
+            if (!visited[*i])
+            {
+                visited[*i] = true;
+                queue.push_back(*i);
+            }
+        }
+    }
+     
+    // If BFS is complete without visiting d
+    return false;
+}
